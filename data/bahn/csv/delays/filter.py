d823b2091075900cfdba3f0c46617970a41fa49d
mendlni88972 <mendlni88972@th-nuernberg.de>
2026-06-19 23:11:40 +0200

    update delay preprocessing to work with more files

--- data/bahn/csv/delays/filter.py
+++ data/bahn/csv/delays/filter.py
@@ -1,35 +1,52 @@
 import pandas as pd
 import os
 
-date = "2021-09-08"
+dates = [
+    "2023-01-28",
+    "2023-01-29",
+    "2023-01-30",
+    "2023-01-31",
+]
+
+# WBA = Waldbahn (kleine bahnen. irrelevant)
+TRAIN_CATEGORIES = {
+    "ICE", "IC", "EC", "RE", "RB", "S", "U", "IRE"
+}
 
 full_path = os.path.realpath(__file__)
 path, _ = os.path.split(full_path)
 
-df = pd.read_parquet(f"{path}/../../raw/delays/2021/{date}.parquet")
-df = df.query("is_final == True")
+stations = set(pd.read_csv(
+    f"{path}/../stations-filtered.csv"
+)["eva"].astype(str))
 
-excluded_stations = pd.read_csv(
-    f"{path}/../stations-excluded.csv"
-)["eva"].astype(str)
-df = df[
-    ~df["stop_id"].astype(str).isin(excluded_stations)
-]
+def convert_date(date: str):
+    delays = pd.read_parquet(f"{path}/../../../../raw/delays/2023/{date}.parquet")
+    delays = delays.query("is_final == True")
+    # df.head(500).to_csv(f"{path}/{date}.csv")
 
-df = df.astype({
-    "trip_id": "uint64",
-    "initial_stop_id": "Int64",
-})
-df = df[[
-    "trip_id",
-    "stop_id",
-    # "initial_stop_id",
-    "stop_sequence",
-    "delay",
-    "category",
-    "operator",
-]]
-# df = df.drop_duplicates()
-# print(df.head(10))
-# df.head(100).to_csv(f"{path}/{date}.csv", index=False)
-df.head(10000).to_csv(f"{path}/{date}.csv", index=False)
+    delays = delays[delays["category"].isin(TRAIN_CATEGORIES)]
+    delays = delays[delays["stop_id"].astype(str).isin(stations)]
+
+    delays = delays.astype({
+        "trip_id": "uint64",
+        "initial_stop_id": "Int64",
+    })
+
+    delays = delays[[
+        "trip_id",
+        "stop_id",
+        # "initial_stop_id",
+        "stop_sequence",
+        "delay",
+        "category",
+        "operator",
+    ]]
+
+    # df = df.drop_duplicates()
+    # print(df.head(10))
+    # df.head(100).to_csv(f"{path}/{date}.csv", index=False)
+    delays.to_csv(f"{path}/{date}.csv", index=False)
+
+for date in dates:
+    convert_date(date)
