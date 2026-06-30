60d7d8cdb3932ea93c26343da5aa1b61615bbed6
Yavor Kyurkchiev <kyurkchiev.yavor@hotmail.com>
2026-06-30 23:02:50 +0200

    Change start date to 01.02.2023

--- src/dateSync.ts
+++ src/dateSync.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_DATE_RANGE = {
-  from: new Date(2025, 0, 1),
-  to: new Date(2025, 0, 12),
+  from: new Date(2023, 1, 1),
+  to: new Date(2023, 1, 14),
 };
 
 export const SELECTED_DATE_CHANGE_EVENT = "selected-date-change";
